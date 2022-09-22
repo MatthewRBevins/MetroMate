328d0963e6db87139babf1e63ea56409df74f64f
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2022-09-22 07:54:34 -0700

    Fixed API key

--- app/src/main/java/com/example/busapp/MapsActivity.java
+++ app/src/main/java/com/example/busapp/MapsActivity.java
@@ -12,6 +12,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.busapp.databinding.ActivityMapsBinding;
 
+import java.net.URL;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
