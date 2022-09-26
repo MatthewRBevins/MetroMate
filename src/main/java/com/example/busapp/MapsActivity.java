666aed8282947192c8a9bdac5044238deb7716f2
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2022-09-26 11:43:56 -0700

    e
    e

--- src/main/java/com/example/busapp/MapsActivity.java
+++ src/main/java/com/example/busapp/MapsActivity.java
@@ -49,6 +49,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    //e
 
     /**
      * Manipulates the map once available.
