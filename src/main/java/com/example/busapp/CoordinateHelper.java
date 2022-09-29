8a38a99bd8ec863e7667d05097ad1e9aaf3df7e7
slickOnGit <milesdz@icloud.com>
2022-09-28 22:24:33 -0700

    added some coord helper and removed unnecessary routes.txt stuff

---
+++ src/main/java/com/example/busapp/CoordinateHelper.java
@@ -0,0 +1,29 @@
+package com.example.busapp;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
+import java.io.IOException;
+
+public class CoordinateHelper {
+    public static Object[] textToCoordinatesAndAddress(String address) {
+        String addressURLFormatted = address.replace(" ","+");
+        String URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+                + addressURLFormatted
+                + "&key=" + BuildConfig.MAPS_API_KEY;
+        Object[] arr = new Object[3];
+        try {
+            String data = Web.readFromWeb(URL);
+            JSONObject json = Web.readJSON(data);
+            JSONObject results = (JSONObject) json.get("results");
+            JSONObject geometry = (JSONObject) results.get("geometry");
+            JSONObject location = (JSONObject) geometry.get("location");
+            arr[0] = (double) location.get("lat"); // latitude
+            arr[1] = (double) location.get("lng"); // longitude
+            arr[2] = (String) results.get("formatted_Address"); // formatted address
+            return arr;
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return arr;
+    }
+}
