e1a9f09ebec5dceb775a98aa54dbe33a1de78113
slickOnGit <milesdz@icloud.com>
2022-09-30 11:38:42 -0700

    the push of the century

--- src/main/java/com/example/busapp/CoordinateHelper.java
+++ src/main/java/com/example/busapp/CoordinateHelper.java
@@ -5,26 +5,34 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.Map;
 
 public class CoordinateHelper {
-    public static Object[] textToCoordinatesAndAddress(String address) {
-        String addressURLFormatted = address.replace(" ","+");
+    public static Object[] textToCoordinatesAndAddress(String text) {
+        String addressURLFormatted = text.replace(" ","+");
         String URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
                 + addressURLFormatted
                 + "&key=" + BuildConfig.MAPS_API_KEY;
-        Object[] arr = new Object[3];
         try {
             String data = Web.readFromWeb(URL);
             JSONObject json = Web.readJSON(new StringReader(data));
             JSONArray resultsArr = (JSONArray) json.get("results");
             if (! resultsArr.isEmpty()) {
-                JSONObject results = (JSONObject) resultsArr.get(0);
-                JSONObject geometry = (JSONObject) results.get("geometry");
-                JSONObject location = (JSONObject) geometry.get("location");
-                arr[0] = (double) location.get("lat"); // latitude
-                arr[1] = (double) location.get("lng"); // longitude
-                arr[2] = (String) results.get("formatted_address"); // formatted address
+                Map<String, Object>[] arr = new HashMap[resultsArr.size()];
+                for (int i = 0; i < resultsArr.size(); i++) {
+                    JSONObject results = (JSONObject) resultsArr.get(i);
+                    JSONObject geometry = (JSONObject) results.get("geometry");
+                    JSONObject location = (JSONObject) geometry.get("location");
+                    Map<String, Object> map = new HashMap<String, Object>();
+                    map.put("latitude", (double) location.get("lat"));
+                    map.put("longitude", (double) location.get("longitude"));
+                    map.put("formatted_address", (String) results.get("formatted_address"))
+                    arr[i] = map;
+                }
                 return arr;
             } else {
                 return null;
