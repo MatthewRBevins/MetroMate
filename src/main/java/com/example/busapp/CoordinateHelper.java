0b4cb17172ab04fc8b2c5d7626797e588656b91b
slickOnGit <milesdz@icloud.com>
2022-09-30 10:59:31 -0700

    more

--- src/main/java/com/example/busapp/CoordinateHelper.java
+++ src/main/java/com/example/busapp/CoordinateHelper.java
@@ -5,6 +5,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Iterator;
 
 public class CoordinateHelper {
     public static Object[] textToCoordinatesAndAddress(String address) {
@@ -23,12 +24,40 @@ public class CoordinateHelper {
                 JSONObject location = (JSONObject) geometry.get("location");
                 arr[0] = (double) location.get("lat"); // latitude
                 arr[1] = (double) location.get("lng"); // longitude
-                arr[2] = (String) results.get("formatted_Address"); // formatted address
+                arr[2] = (String) results.get("formatted_address"); // formatted address
                 return arr;
+            } else {
+                return null;
             }
         } catch (IOException | ParseException e) {
             e.printStackTrace();
+            return null;
         }
-        return arr;
+    }
+
+    public static Object[] coordinatesToAddress(double longitude, double latitude, int resultCount) {
+        String latlng = String.valueOf(longitude) + ", " + String.valueOf(latitude);
+        String latlngURLFormatted = latlng.replace(" ","+");
+        String URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="
+                + latlngURLFormatted
+                + "&key=" + BuildConfig.MAPS_API_KEY;
+        try {
+            String data = Web.readFromWeb(URL);
+            JSONObject json = Web.readJSON(new StringReader(data));
+            JSONArray resultsArr = (JSONArray) json.get("results");
+            if (! resultsArr.isEmpty()) {
+                String[] arr = new String[resultsArr.size()];
+                for (int i = 0; i < resultsArr.size(); i++) {
+                    JSONObject results = (JSONObject) resultsArr.get(i);
+                    arr[i] = (String) results.get("formatted_address");
+                }
+                return arr;
+            } else {
+                return null;
+            }
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
